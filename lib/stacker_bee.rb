527260f04a2ecc5c59a9fad132d8f3fa18fc63b0
Greg Sterndale <gsterndale@gmail.com>
2013-10-10 14:15:24 -0400

    Require pattern

--- lib/stacker_bee.rb
+++ lib/stacker_bee.rb
@@ -1,5 +1,14 @@
-require "stacker_bee/version"
+require_stacker_bee = if defined?(require_relative)
+  lambda do |path|
+    require_relative path
+  end
+else # for 1.8.7
+  lambda do |path|
+    require "stacker_bee/#{path}"
+  end
+end
+
+require_stacker_bee['stacker_bee/version']
 
 module StackerBee
-  # Your code goes here...
 end
