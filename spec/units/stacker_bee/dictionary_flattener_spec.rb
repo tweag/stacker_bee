e63965d01846734de39aa64b3e876fb7303d7419
Len Smith <len@barrison.com>
2013-12-23 13:53:39 -0500

    Remove empty hash value in map parameters

--- spec/units/stacker_bee/dictionary_flattener_spec.rb
+++ spec/units/stacker_bee/dictionary_flattener_spec.rb
@@ -18,16 +18,25 @@ describe StackerBee::DictionaryFlattener do
 
   describe ".new" do
     let(:prefix) { StackerBee::DictionaryFlattener.tokenize "rebels[0]" }
-    let(:params) { { "time" => "long ago", "rebels" => { "r2" => "d2" } } }
+    let(:prefix2) { StackerBee::DictionaryFlattener.tokenize "rebels[1]" }
+
+    let(:params) do
+      { "time" => "long ago",
+        "rebels" => { "r2" => "d2", "r1" => "" } }
+    end
 
     subject do
       StackerBee::DictionaryFlattener.new(params).params
     end
 
     it "flattens objects in the manner that cloudstack expects" do
-      subject["#{prefix}.name"].should == "r2"
-      subject["#{prefix}.key"].should == "r2"
-      subject["#{prefix}.value"].should == "d2"
+      subject["#{prefix}.name"].should eq "r2"
+      subject["#{prefix}.key"].should eq "r2"
+      subject["#{prefix}.value"].should eq "d2"
+    end
+
+    it "doesnt flatten empty hashes" do
+      subject.should_not have_key "#{prefix2}.name"
     end
 
     it "removes original map params" do
