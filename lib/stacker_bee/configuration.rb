446bce32dd6940020c7a87beaa6466791a2706af
Mike Nicholaides <mike.nicholaides@gmail.com>
2014-04-12 15:51:54 -0400

    Ignore necessary rubocop violation

--- lib/stacker_bee/configuration.rb
+++ lib/stacker_bee/configuration.rb
@@ -3,6 +3,8 @@ require "ostruct"
 module StackerBee
   class Configuration < OpenStruct
     def ssl_verify?
+      # rubocop:disable NonNilCheck
+      # it should default to true if it's not explicitly set
       !ssl_verify.nil? ? ssl_verify : true
     end
   end
