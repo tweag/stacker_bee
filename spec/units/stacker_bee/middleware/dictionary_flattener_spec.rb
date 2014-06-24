640d1950cdbc1abe09e6d8f6e36358d7b4b14388
Len Smith <len@barrison.com>
2014-06-24 11:48:07 -0400

    Removing 'false' values from dictionary params

--- spec/units/stacker_bee/middleware/dictionary_flattener_spec.rb
+++ spec/units/stacker_bee/middleware/dictionary_flattener_spec.rb
@@ -19,31 +19,43 @@ describe StackerBee::Middleware::DictionaryFlattener do
   describe ".new" do
     subject { described_class.new.params(params) }
 
-    def prefix(number)
+    def param(number)
       described_class.tokenize "rebels[#{number}]"
     end
 
     let(:params) do
       { "time" => "long ago",
-        "rebels" => { "r2" => "d2", "r1" => "", "father" => false } }
+        "rebels" => {
+          "r2"      => "d2",
+          "blank"   => "",
+          "falsey"  => "false",
+          "false"   => false,
+          "droid"   => true } }
     end
 
     it "flattens objects in the manner that cloudstack expects" do
-      subject["#{prefix(0)}.name"].should eq "r2"
-      subject["#{prefix(0)}.key"].should eq "r2"
-      subject["#{prefix(0)}.value"].should eq "d2"
+      subject["#{param(0)}.name"].should eq "r2"
+      subject["#{param(0)}.key"].should eq "r2"
+      subject["#{param(0)}.value"].should eq "d2"
     end
 
     it "does not flatten empty hashes" do
-      subject.should_not have_key "#{prefix(2)}.name"
+      subject.should_not have_key "#{param(2)}.name"
     end
 
-    it "handles booleans" do
-      subject["#{prefix(1)}.name"].should eq "father"
+    it "handles true booleans" do
+      subject["#{param(1)}.name"].should eq "droid"
+      subject["#{param(1)}.value"].should be_true
     end
 
     it "removes original map params" do
       subject.should_not have_key "rebels"
+    end
+
+    it "doesnt send false to CloudStack" do
+      # This assumption is based on CloudStack UI behavior
+      subject.values.should_not include false
+      subject.values.should_not include "false"
     end
   end
 end
