ce0a5c1ec4e574cda02e9d46d004d543ca561a1c
Mike Nicholaides <mike.nicholaides@gmail.com>
2014-07-20 13:08:43 -0400

    Fix rspec configuration deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,6 @@ support_files = Dir[File.join(
 support_files.each { |f| require f }
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
